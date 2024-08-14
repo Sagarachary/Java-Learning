8962bb4e9abbc85b5f4cbc69847691464e7e65b9
Sagarachary <acharysagar01@gmail.com>
2024-08-14 15:01:38 +0530

    changes on count sort

--- Java/Day8/Countingsort.java
+++ Java/Day8/Countingsort.java
@@ -1,35 +1,35 @@
 package Day8;
 public class Countingsort {
-    public static void countingSort(int arr[]){
+    public static void countingSort(int number[]){
         int largest = Integer.MIN_VALUE;
-        for(int i=0; i<arr.length; i++){
-           largest = Math.max(largest,arr[i]);
+        for(int i=0; i<number.length; i++){
+           largest = Math.max(largest,number[i]);
         }
         int count[] = new int[largest+1];
-        for(int i=0; i<arr.length; i++){
-            count[arr[i]]++;
+        for(int i=0; i<number.length; i++){
+            count[number[i]]++;
         }
 
         // sorting
         int j=0;
         for(int i=0; i<count.length; i++){
             while(count[i]>0){
-                arr[j] =i;
+                number[j] =i;
                 j++;
                 count[i]--;
             }
         }
     }
 
-    public static void printArr(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+    public static void printArr(int number[]){
+        for(int i=0;i<number.length;i++){
+            System.out.print(number[i]+" ");
         }
         System.out.println();
     }
     public static void main(String[] args){
-        int arr[] ={5,4,1,3,2};
-        countingSort(arr);
-        printArr(arr);
+        int number[] ={5,4,1,3,2};
+        countingSort(number);
+        printArr(number);
     }
 }
