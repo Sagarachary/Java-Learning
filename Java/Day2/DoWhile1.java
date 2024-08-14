24d537fb1a9b0dc2b8295841995e72dd11d61def
Sagarachary <acharysagar01@gmail.com>
2024-08-14 15:03:07 +0530

    changes on do while

--- Java/Day2/DoWhile1.java
+++ Java/Day2/DoWhile1.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class DoWhile1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(final String[] args){
+        final Scanner sc = new Scanner(System.in);
         int value = 0;
         do{
             System.out.println("Enter a number: ");
